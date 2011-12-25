034cef3ac91284692d9022873723f2756344d2ac
djordje <djordje.bigjoe@gmail.com>
2011-12-25 03:38:06 +0100

    Filesystem adapter in new namespace. Now it extends lithium core Object.

---
+++ extensions/data/source/Filesystem.php
@@ -0,0 +1,163 @@
+<?php
+
+namespace li3_filemanager\extensions\data\source;
+
+/**
+ * Filesystem data source/adapter
+ * _init() `chdir()` to defined location so all other actions take predifined location
+ * as a root. All actions are named by UNIX bash command names
+ * This object abstract default PHP functions so we can perform recursive copy and remove
+ */
+class Filesystem extends \lithium\core\Object {
+	
+	/**
+	 * Change current dir to one defined in location passed as config array
+	 */
+	protected function _init() {
+		parent::_init();
+		chdir($this->_config['location']);
+	}
+	
+	/**
+	 * Pass filter to ls and you'll get file and dir list that mach pattern or FALSE
+	 * This method is relative to currrent dir (defined in location passed to constructor)
+	 * @example
+	 *
+	 *		ls('*')         ---> everything in root dir
+	 *		ls('li/*')      ---> everything in root/li3 dir
+	 *		ls('img/*.jpg') ---> every file with `.jpg` extension in root/img dir
+	 * @param string $filter
+	 * @return mixed (boolean FALSE or array of paths)
+	 */
+	public function ls($ls) {
+		if ($glob = glob($ls)) {
+			return $glob;
+		} else {
+			$parrent = function($path) {
+				$split = explode('/', $path);
+				$end = count($split) - 1;
+				unset ($split[$end]);
+				if (empty ($split)) {
+					return '.';
+				} else {
+					return join('/', $split);
+				}
+			};
+			$parrent($ls);
+			if (is_dir($parrent($ls))) {
+				return array();
+			} else {
+				return FALSE;
+			}
+		}
+	}
+	
+	/**
+	 * Make new directory on passed path
+	 * Path is relative ro root defined by constructor
+	 * @param string $name
+	 * @return boolean
+	 */
+	public function mkdir($name) {
+		return mkdir($name);
+	}
+	
+	/**
+	 * This method expand default PHP copy to enable recursive copy
+	 * Passed source and destination is relative to root defined by constructor
+	 * @param string $src
+	 * @param string $dst
+	 * @return boolean
+	 */
+	public function cp($src, $dst) {
+		if (is_file($src)) {
+			return copy($src, $dst);
+		} else if(is_dir($src)) {
+			mkdir($dst);
+			$content = glob($src.'/*');
+			foreach($content as $path) {
+				$splitSrc = explode('/', $path);
+				$splitDst = explode('/', $dst);
+				$subDst = join('/', $splitDst + $splitSrc);
+				$this->cp($path, $subDst);
+			}
+			return TRUE;
+		} else {
+			return FALSE;
+		}
+	}
+	
+	/**
+	 * This is wrapper for default PHP rename function
+	 * We use this method for rename and move of a file or dir
+	 * Passed paths are relative to root defined by constructor
+	 * @param string $from
+	 * @param string $to
+	 * @return boolean
+	 */
+	public function mv($from, $to) {
+		return rename($from, $to);
+	}
+	
+	/**
+	 * Pass path of file or empty dir as a param and remove it
+	 * This method implements combination of PHP functions `unlink()` and `rmdir()`
+	 * Passed paths are relative to root defined by constructor
+	 * @param string $path
+	 * @return boolean
+	 */
+	public function rm($path) {
+		if (file_exists($path)) {
+			if (is_dir($path)) {
+				return rmdir($path);
+			} else {
+				return unlink($path);
+			}
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * This method enables recursive remove (removing dir with content)
+	 * This is wrapper for `$this->rm()` but it loop trough content and
+	 * remove things in right order
+	 * Passed paths are relative to root defined by constructor
+	 * @param string $path
+	 * @return boolean
+	 */
+	public function rmR($path) {
+		if (is_dir($path)) {
+			$content = $this->ls($path.'/*');
+			foreach ($content as $file) {
+				$this->rmR($file);
+			}
+			return $this->rm($path);
+		} else {
+			$this->rm($path);
+		}
+	}
+	
+	/**
+	 * This method move uploaded files to specified destination
+	 * We pass array of posted files and desired destination and this method
+	 * loop trough array and move files by PHP `move_uploaded_files()` function
+	 * Passed destination path is relative to root defined by constructor
+	 * @param array $postedFiles
+	 * @param string $dst
+	 * @return boolean
+	 */
+	public function upload($postedFiles, $dst) {
+		foreach ($postedFiles as $file) {
+			if ($file['error'] == 0 && is_dir($dst)) {
+				move_uploaded_file($file['tmp_name'], $dst.'/'.$file['name']);
+			} else {
+				return FALSE;
+			}
+		}
+		return TRUE;
+	}
+	
+}
+
+?>
