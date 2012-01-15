11c0a7a2a773b039ffdcf5c5a30b610f4e96769f
djordje <djordje.bigjoe@gmail.com>
2012-01-15 01:15:03 +0100

    Added test case for `Locations`

---
+++ tests/cases/extensions/data/LocationsTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace li3_filemanager\tests\cases\extensions\data;
+use li3_filemanager\extensions\data\Locations;
+
+class LocationsTest extends \lithium\test\Unit {
+
+	public function  testAdd() {
+		$this->assertTrue(is_array(
+			Locations::add('test', array(
+				'adapter' => 'Filesystem',
+				'location' => LITHIUM_APP_PATH . '/libraries/li3_filemanager/resources/fs'
+			))
+		));
+	}
+
+	public function testGet() {
+		$this->assertTrue(Locations::get(false));
+		$this->assertTrue(is_array(Locations::get()));
+		$this->assertNull(Locations::get('test2'));
+		$this->assertTrue(is_array(Locations::get('test', array('config' => true))));
+		$this->assertNull(Locations::get('test', array('autoCreate' => false)));
+		$this->assertTrue(Locations::get('test'));
+	}
+
+}
+
+?>
