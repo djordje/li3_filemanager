7f48bf0c15e2f6f0b51973aa8cb2025e79cbc70b
Djordje Kovacevic <djordje.bigjoe@gmail.com>
2012-06-10 05:46:41 +0200

    Removed all files on `dev` branch, preparing to rewrite

--- views/file/mkdir.html.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$this->title('Make directory');
-?>
-
-<?=$this->html->link('Cancel', $this->_request->env('HTTP_REFERER')); ?>
-<?=$this->form->create(); ?>
-<?=$this->form->field('name'); ?>
-<?=$this->form->submit('Create'); ?>
-<?=$this->form->end(); ?>
