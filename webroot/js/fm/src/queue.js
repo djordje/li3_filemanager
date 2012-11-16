86deefe3b69603dd9dd9643466311881b9f307aa
djordje <djordje.bigjoe@gmail.com>
2012-11-16 19:59:33 +0100

    Updated `Queue` JS object

--- webroot/js/fm/src/queue.js
+++ webroot/js/fm/src/queue.js
@@ -1,9 +1,8 @@
 FileManager.Queue = {
 	lastAction: '',
 	allowedActions: ['copy', 'move', 'remove'],
-	actionExists: false,
 	queued: [],
-	
+
 	add: function(action, value, reset) {
 		var exists = false;
 		if (!action || !value) {
@@ -13,34 +12,32 @@ FileManager.Queue = {
 			this.reset();
 		}
 		for (var i = this.allowedActions.length - 1; i >= 0; i--) {
-			if (this.allowedActions[i] === action)
-				this.actionExists = true;
-		}
-		if (!this.actionExists) {
-			return false;
-		}
-		this.lastAction = action;
-		this.actionExists = false;
-		for (var n = this.queued.length - 1; n >= 0; n--) {
-			if (this.queued[n] === value) {
-				exists = true;
+			if (this.allowedActions[i] === action) {
+				this.lastAction = action;
+				for (var n = this.queued.length - 1; n >= 0; n--) {
+					if (this.queued[n] === value) {
+						exists = true;
+					}
+				}
 			}
 		}
-		if (!exists)
+		if (!exists) {
 			return this.queued.push(value);
+		}
 		return false;
 	},
-	
+
 	remove: function(value) {
-		if (this.last_action === '')
+		if (this.last_action === '') {
 			return false;
+		}
 		for (var i = this.queued.length - 1; i >= 0; i--) {
 			if (this.queued[i] === value) {
 				return !this.queued.splice(i, 1);
 			}
 		}
 	},
-	
+
 	reset: function() {
 		this.lastAction = '';
 		this.actionExists = false;
